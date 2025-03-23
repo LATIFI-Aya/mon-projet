a8c667d6b9be3b1c73434ac1b6f76bf19e7a50d7
skyzo-free <ayalatifi6@gmail.com>
2025-03-23 16:02:26 +0000

    Create POST LIST

--- frontend/src/components/FeaturedPosts.jsx
+++ frontend/src/components/FeaturedPosts.jsx
@@ -9,7 +9,7 @@ const FeaturedPosts = () => {
       {/* First */}
       <div className='w-full lg:w-1/2 flex flex-col gap-4 '>
       {/* images */}
-      <Image src="featured1.jpeg" className="rounded-3xl object-cover"/>
+      <Image src="featured1.jpeg" className="rounded-3xl object-cover" w="895"/>
       {/* details */}
       <div className='flex items-center gap-4'>
         <h1 className='font-semibold lg:text-lg'>01.</h1>
@@ -23,7 +23,13 @@ const FeaturedPosts = () => {
       <div className='w-full lg:h-1/2 flex flex-col gap-4'> 
         {/* second */}
         <div className='lg:h-1/3 flex justify-between gap-4'>
-        <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+        <div className='w-1/3 aspect-video'>
+            <Image 
+              src="featured2.jpeg" 
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+        </div>
         {/* details and title */}
           <div className='w-2/3 '>
           {/* details  */}
@@ -38,8 +44,13 @@ const FeaturedPosts = () => {
         </div>
         {/* third */}
         <div className='lg:h-1/3 flex justify-between gap-4'>
-        <div className='lg:h-1/3 flex justify-between gap-4'>
-        <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+        <div className='w-1/3 aspect-video'>
+            <Image 
+              src="featured3.jpeg" 
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+        </div>
         {/* details and title */}
           <div className='w-2/3 '>
           {/* details  */}
@@ -52,11 +63,16 @@ const FeaturedPosts = () => {
           <Link to="/test" className='text-base sm:text-lg md:text-2xl lg:text-xl xl:text-2xl font-medium'>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</Link>
           </div>
         </div>
-        </div>
+        
         {/* fourth */}
         <div className='lg:h-1/3 flex justify-between gap-4'> 
-        <div className='lg:h-1/3 flex justify-between gap-4'>
-        <Image src="featured2.jpeg" className="rounded-3xl object-cover w-1/3 aspect-video" />
+        <div className='w-1/3 aspect-video'>
+            <Image 
+              src="featured4.jpeg" 
+              className="rounded-3xl object-cover w-full h-full"
+              w="298"
+            />
+        </div>
         {/* details and title */}
           <div className='w-2/3 '>
           {/* details  */}
@@ -71,7 +87,7 @@ const FeaturedPosts = () => {
         </div>
         </div>
       </div>
-    </div>
+    
   )
 }
 
--- frontend/src/routes/Homepage.jsx
+++ frontend/src/routes/Homepage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import MainCategories from '../components/MainCategories'
 import FeaturedPosts from '../components/FeaturedPosts'
+import PostList from '../components/PostList'
 
 const Homepage = ()=>{
   return (
@@ -69,6 +70,11 @@ const Homepage = ()=>{
         <MainCategories />
         {/* FEATURED POSTS */}
         <FeaturedPosts />
+        {/* POST LIST */}
+        <div className=''>
+          <h1 className='my_8 text-2xl text-gray-600'>Recent Posts</h1>
+          <PostList />
+        </div>
     </div>
   )
 }
